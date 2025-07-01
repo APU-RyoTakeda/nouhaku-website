05012e2c5bbc5d679d3e3f28bcfa1bf5582f10bb
あなたの名前 <@example.com>
2025-07-01 17:14:56 +0900

    一旦

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -37,8 +37,6 @@ export default function Home() {
           <div className="mt-16">
             <FeaturesImageTextSection />
           </div>
-          <HouseSpotlightSection />
-          <TownSpotlightSection />
 
         </>
       )}
